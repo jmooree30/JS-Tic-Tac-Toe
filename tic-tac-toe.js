dc88a14968e0bdeb533ca0070f16798ae67872a0
Jacob <jmooree30@gmail.com>
2018-01-22 22:02:35 -0500

    choice added

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -11,12 +11,32 @@ const Player = (name,piece) => {
 
 function grid(){
   for(i=0;i<gameboard.board.length;i++){
-    console.log(gameboard.board.length)
     var div = document.createElement('div');
     div.innerHTML=gameboard.board[i]
     div.classList.add("mystyle")
+    div.dataset.number = i;
     document.querySelector('.container').appendChild(div);
+  }
+  choice()
+}
+
+function removeGrid(){
+  container = document.querySelector(".container")
+  divs = document.querySelector(".mystyle")
+  while(container.hasChildNodes()){
+    container.removeChild(container.firstChild)
   }
 }
 
 grid()
+
+function choice(){
+clickables = document.querySelectorAll(".mystyle")
+for(i=0;i<clickables.length;i++){
+  clickables[i].addEventListener("click", function(e){
+  removeGrid()
+  gameboard.board[e.target.dataset.number] = "X"
+  grid()
+});
+} 
+}
