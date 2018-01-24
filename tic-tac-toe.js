e388905583dfe51c19ff20979da1196c75bacd79
Jacob <jmooree30@gmail.com>
2018-01-23 23:50:47 -0500

    game logic finished

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,5 +1,5 @@
 const gameboard = (() => {
-  board = ["x","o","x","o","x","o","x","o","x"]
+  board = ["","","","","","","","",""]
   return {board}
 })(); 
 
@@ -11,7 +11,7 @@ const Player = (name,piece) => {
 
 player1 = Player("Jacob","X")
 player2 = Player("Vlad","O")
-console.log(player1.name)
+let counter = 2
 
 function grid(){
   for(i=0;i<gameboard.board.length;i++){
@@ -21,7 +21,7 @@ function grid(){
     div.dataset.number = i;
     document.querySelector('.container').appendChild(div);
   }
-  choice()
+  choice(player1,player2)
 }
 
 function removeGrid(){
@@ -32,16 +32,65 @@ function removeGrid(){
   }
 }
 
-function choice(){
+function checkUsed(e){
+  return gameboard.board[e.target.dataset.number] != "" ? true : false;
+}
+
+function choice(player1, player2){
   clickables = document.querySelectorAll(".mystyle")
   for(i=0;i<clickables.length;i++){
     clickables[i].addEventListener("click", function(e){
       removeGrid()
-      gameboard.board[e.target.dataset.number] = "X"
-      grid()
-    });
+      if (checkUsed(e)){alert('nope')}
+        else if (counter %2 == 0){
+          gameboard.board[e.target.dataset.number] = player1.piece
+          counter +=1;
+          checkWin(player1.piece)
+        }
+        else{
+          gameboard.board[e.target.dataset.number] = player2.piece
+          counter +=1;
+          checkWin(player2.piece)
+        }
+        grid()
+      });
   } 
 }
 
-grid()
+function checkWin(player){
+  const winConditions = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
+  ];
 
+  for(i=0;i<winConditions.length;i++){
+    if (gameboard.board[winConditions[i][0]] == player &&
+      gameboard.board[winConditions[i][1]] == player &&
+      gameboard.board[winConditions[i][2]] == player){
+      alert("You win!")
+    break;
+  }
+}
+if (!gameboard.board.includes("")){
+  alert("tie game!");
+}
+}
+
+function newGame(){
+  gameboard.board = ["","","","","","","","",""];
+  removeGrid();
+  grid();
+}
+
+restart = document.getElementById("restart")
+restart.addEventListener("click",function(){
+  newGame()
+})
+
+grid()
