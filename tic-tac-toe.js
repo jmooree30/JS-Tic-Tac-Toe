7b48b4cba49526333b1eca14ea3d9dfe1594ede5
Jacob <jmooree30@gmail.com>
2018-01-25 19:36:33 -0500

    Complete

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -8,24 +8,23 @@ const Player = (name,piece) => {
  piece = piece
  return {name, piece}
 };
+player1 = Player("","X")
+player2 = Player("","O")
 
-player1 = Player("Jacob","X")
-player2 = Player("Vlad","O")
 let counter = 2
-
 function grid(){
   for(i=0;i<gameboard.board.length;i++){
     var div = document.createElement('div');
     div.innerHTML=gameboard.board[i]
     div.classList.add("mystyle")
     div.dataset.number = i;
-    document.querySelector('.container').appendChild(div);
+    document.querySelector('.containers').appendChild(div);
   }
   choice(player1,player2)
 }
 
 function removeGrid(){
-  container = document.querySelector(".container")
+  container = document.querySelector(".containers")
   divs = document.querySelector(".mystyle")
   while(container.hasChildNodes()){
     container.removeChild(container.firstChild)
@@ -37,27 +36,31 @@ function checkUsed(e){
 }
 
 function choice(player1, player2){
-  clickables = document.querySelectorAll(".mystyle")
+  playerturn = document.querySelector("p")
+  if(counter == 2){playerturn.innerHTML = player1.name + "'s turn"}
+    clickables = document.querySelectorAll(".mystyle")
   for(i=0;i<clickables.length;i++){
     clickables[i].addEventListener("click", function(e){
       removeGrid()
       if (checkUsed(e)){alert('nope')}
         else if (counter %2 == 0){
           gameboard.board[e.target.dataset.number] = player1.piece
+          playerturn.innerHTML = player2.name + "'s turn"
           counter +=1;
-          checkWin(player1.piece)
+          checkWin(player1.piece,player1.name)
         }
         else{
           gameboard.board[e.target.dataset.number] = player2.piece
+          playerturn.innerHTML = player1.name + "'s turn"
           counter +=1;
-          checkWin(player2.piece)
+          checkWin(player2.piece,player2.name)
         }
         grid()
       });
   } 
 }
 
-function checkWin(player){
+function checkWin(player, playername){
   const winConditions = [
   [0, 1, 2],
   [3, 4, 5],
@@ -73,11 +76,21 @@ function checkWin(player){
     if (gameboard.board[winConditions[i][0]] == player &&
       gameboard.board[winConditions[i][1]] == player &&
       gameboard.board[winConditions[i][2]] == player){
-      alert("You win!")
-      win = true 
+      swal({
+        title: "Good job!",
+        text: playername + " wins!",
+        icon: "success",
+        button: "New game",
+      })
+    .then((value) => {
+      swal(window.location.reload(true));
+    });
+    
+    win = true 
   }
 }
 if (!gameboard.board.includes("") && win != true){
+  newGame()
   alert("tie game!");
 }
 }
@@ -93,4 +106,18 @@ restart.addEventListener("click",function(){
   newGame()
 })
 
-grid()
+
+document.querySelector("form").addEventListener("submit", function(e){
+  e.preventDefault()
+  grids = document.getElementsByClassName("mystyle")
+  for(i=0;i<grids.length;i++){
+    grids[i].style.display = "inline"
+  }
+  form = document.querySelector("form")
+  form.style.display = "none"
+  playerone = document.getElementById("player1").value
+  player1.name = playerone
+  playertwo = document.getElementById("player2").value
+  player2.name = playertwo
+  grid()
+})
